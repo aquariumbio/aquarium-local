492669b07e77e22c471b349a2ff52a93ec11171a
Ben Keller <bjkeller@uw.edu>
2020-02-13 07:35:52 -0800

    Update to match changes to aquarium

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 key_base=`openssl rand -hex 64`
 s3_key=`openssl rand -hex 40`
+timezone=`curl https://ipapi.co/timezone`
 ENV_FILE=.env
 
 if [[ ! -f "$ENV_FILE" ]]; then
@@ -10,6 +11,6 @@ if [[ ! -f "$ENV_FILE" ]]; then
     echo 'S3_SERVICE=minio' >> $ENV_FILE
     echo 'S3_ID=aquarium_minio' >> $ENV_FILE
     echo 'S3_SECRET_ACCESS_KEY='$s3_key >> $ENV_FILE
-    echo 'TIMEZONE=America/Los_Angeles' >> $ENV_FILE
+    echo 'TIMEZONE='$timezone >> $ENV_FILE
     echo 'SECRET_KEY_BASE='$key_base >> $ENV_FILE 
 fi
