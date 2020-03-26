ad6c24f5bde53719d717a8cb7969799e985dd5e9
Ben Keller <bjkeller@uw.edu>
2020-03-26 09:05:06 -0700

    Fix startup script

--- aquarium.sh
+++ aquarium.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if [ $1 = "up" ]
+if [ $1 = "up" ]; then
     docker-compose pull && docker-compose $@
-elif [ $1 = "down" ]
+elif [ $1 = "down" ]; then
     docker-compose $@ -v
 else
     docker-compose $@
