83c718a48bc1e08c61867290428712a15566b130
Ben Keller <bjkeller@uw.edu>
2020-03-26 09:03:23 -0700

    Add conditional to start script

--- aquarium.sh
+++ aquarium.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-docker-compose pull && docker-compose $@
+if [ $1 = "up" ]
+    docker-compose pull && docker-compose $@
+elif [ $1 = "down" ]
+    docker-compose $@ -v
+else
+    docker-compose $@
+fi
